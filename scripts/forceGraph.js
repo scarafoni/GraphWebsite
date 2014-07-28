d4dfc6cf38d858c89dbb0ca1a54e5222c6b5f502
scarafoni <dan@scarafoni.com>
2014-07-27 20:32:21 -0400

    standard force grah json

--- scripts/forceGraph.js
+++ scripts/forceGraph.js
@@ -1,146 +1,47 @@
- var json = {
-    "nodes":
-        {
-        name:'RootNode',
-        children:[
-        {
-            "switch_id": 1,
-            "name": "switch1",
-            "children": [
-                {
-                    "name": "port-1"
-                },
-                {
-                    "name": "port-2"
-                }
-            ]
-        },
-        {
-            "switch_id": 2,
-            "name": "switch2"
-        },
-        {
-            "switch_id": 3,
-            "name": "switch3"
-        }
-        ]
-        }
-    ,
-    "links":[
-       {
-          "source": 1,
-          "target": 2,
-          "value": 5
-       }
-     ]
-};
-
-
-
 var width = 960,
-    height = 500,
-    root;
+    height = 500;
+
+var color = d3.scale.category20();
 
 var force = d3.layout.force()
-    .size([width, height])
-    .on("tick", tick);
+    .charge(-120)
+    .linkDistance(30)
+    .size([width, height]);
 
 var svg = d3.select("body").append("svg")
     .attr("width", width)
     .attr("height", height);
 
-var link = svg.selectAll(".link"),
-    node = svg.selectAll(".node");
-
-/*d3.json("readme.json", function(json) {
-  root = json;
-  update();
-});*/
-
-// Now get at the nodes you want displayed
-root = json.nodes;
-update();
-
-function update() {
-  var nodes = flatten(root),
-      links = d3.layout.tree().links(nodes);
-
-  // Restart the force layout.
+d3.json("miserables.json", function(error, graph) {
   force
-      .nodes(nodes)
-      .links(links)
+      .nodes(graph.nodes)
+      .links(graph.links)
       .start();
 
-  // Update the links…
-  link = link.data(links, function(d) { return d.target.id; });
-
-  // Exit any old links.
-  link.exit().remove();
-
-  // Enter any new links.
-  link.enter().insert("line", ".node")
+  var link = svg.selectAll(".link")
+      .data(graph.links)
+    .enter().append("line")
       .attr("class", "link")
-      .attr("x1", function(d) { return d.source.x; })
-      .attr("y1", function(d) { return d.source.y; })
-      .attr("x2", function(d) { return d.target.x; })
-      .attr("y2", function(d) { return d.target.y; });
+      .style("stroke-width", function(d) { return Math.sqrt(d.value); });
 
-  // Update the nodes…
-  node = node.data(nodes, function(d) { return d.id; }).style("fill", color);
-
-  // Exit any old nodes.
-  node.exit().remove();
-
-  // Enter any new nodes.
-  node.enter().append("circle")
+  var node = svg.selectAll(".node")
+      .data(graph.nodes)
+    .enter().append("circle")
       .attr("class", "node")
-      .attr("cx", function(d) { return d.x; })
-      .attr("cy", function(d) { return d.y; })
-      .attr("r", function(d) { return Math.sqrt(d.size) / 10 || 4.5; })
-      .style("fill", color)
-      .on("click", click)
+      .attr("r", 5)
+      .style("fill", function(d) { return color(d.group); })
       .call(force.drag);
-}
 
-function tick() {
-  link.attr("x1", function(d) { return d.source.x; })
-      .attr("y1", function(d) { return d.source.y; })
-      .attr("x2", function(d) { return d.target.x; })
-      .attr("y2", function(d) { return d.target.y; });
+  node.append("title")
+      .text(function(d) { return d.name; });
 
-  node.attr("cx", function(d) { return d.x; })
-      .attr("cy", function(d) { return d.y; });
-}
+  force.on("tick", function() {
+    link.attr("x1", function(d) { return d.source.x; })
+        .attr("y1", function(d) { return d.source.y; })
+        .attr("x2", function(d) { return d.target.x; })
+        .attr("y2", function(d) { return d.target.y; });
 
-// Color leaf nodes orange, and packages white or blue.
-function color(d) {
-  return d._children ? "#3182bd" : d.children ? "#c6dbef" : "#fd8d3c";
-}
-
-// Toggle children on click.
-function click(d) {
-  if (!d3.event.defaultPrevented) {
-    if (d.children) {
-      d._children = d.children;
-      d.children = null;
-    } else {
-      d.children = d._children;
-      d._children = null;
-    }
-    update();
-  }
-}
-
-// Returns a list of all nodes under the root.
-function flatten(root) {
-  var nodes = [], i = 0;
-
-  function recurse(node) {
-    if (node.children) node.children.forEach(recurse);
-    if (!node.id) node.id = ++i;
-    nodes.push(node);
-  }
-
-  recurse(root);
-  return nodes;
-}
+    node.attr("cx", function(d) { return d.x; })
+        .attr("cy", function(d) { return d.y; });
+  });
+});
